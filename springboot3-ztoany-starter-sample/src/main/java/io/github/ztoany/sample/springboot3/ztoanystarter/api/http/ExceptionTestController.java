53e001b0336e2e2da9fc0e8e65fb01f82ec2ba73
ztoany <marvin.m.huang@outlook.com>
2025-03-25 10:46:52 +0800

    upgrade springboot version to 3.4.4

--- springboot3-ztoany-starter-sample/src/main/java/io/github/ztoany/sample/springboot3/ztoanystarter/api/http/ExceptionTestController.java
+++ springboot3-ztoany-starter-sample/src/main/java/io/github/ztoany/sample/springboot3/ztoanystarter/api/http/ExceptionTestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public class ExceptionTestController {
     @GetMapping("/business-exceptions")
     public String businessException() {
-        throw ExceptionBuilder.entityNotFoundException("客户", 1);
+        throw ExceptionBuilder.entityNotFoundException("customer", 1);
     }
 
     @GetMapping("/unhandled-exceptions")
